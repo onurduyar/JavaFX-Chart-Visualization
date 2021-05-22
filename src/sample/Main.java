68099001328aa9b4c13274aada3247660f8d0d03
Ahmet Saraç <ahmet.src.99@gmail.com>
2021-05-22 20:47:05 +0300

    Window title changed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Chart Visualization");
         primaryStage.setScene(new Scene(root, 800, 700));
         primaryStage.show();
     }
