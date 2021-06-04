776227d0cd2bf56104aa27401518d4195b9ba874
Ahmet Saraç <ahmet.src.99@gmail.com>
2021-06-04 18:28:41 +0300

    bar chart animation is added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,21 @@
 package sample;
 
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.scene.layout.*;
 
 import java.io.File;
 import java.io.IOException;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import javafx.animation.*;
 import javafx.fxml.FXML;
@@ -42,7 +49,7 @@ public class Controller {
     private RadioButton barRadio = new RadioButton();
 
     @FXML
-    private GridPane grid =new GridPane();
+    private FlowPane flowPane =new FlowPane();
 
     @FXML
     private VBox vBox = new VBox();
@@ -50,6 +57,8 @@ public class Controller {
     @FXML
     private VBox vBoxText = new VBox();
 
+    private Color[] colorArray = {Color.web("#008080"), Color.web("#556b2f"), Color.web("#800000"), Color.web("#483d8b"), Color.web("#008000"), Color.web("#008080"), Color.web("#b8860b"), Color.web("#000080"), Color.web("#9acd32"), Color.web("#8b008b"), Color.web("#ff0000"), Color.web("#00ced1"), Color.web("#00ff00"), Color.web("#00fa9a"), Color.web("#8a2be2"), Color.web("#00ffff"), Color.web("#00bfff"), Color.web("#0000ff"), Color.web("#ff00ff"), Color.web("#1e90ff"), Color.web("#db7093"), Color.web("#f0e68c"), Color.web("#ff1493"), Color.web("#ffa07a"), Color.web("#ee82ee")};
+    private HashMap<String, Color> colorMap= new HashMap<String, Color>();
 
     public void readFile() throws IOException {
         FileChooser fileChooser = new FileChooser();
@@ -213,56 +222,93 @@ public class Controller {
             }
         }
     }
+
+
+    int i, k = 0;
+    Text barText;
+
     public void createChart(){
         ArrayList<Record> topTen = new ArrayList<Record>();
         long  maxValue;
         maxValue = data.type.equals("txt") ?  (data.records.get(data.records.size()-data.recordsNumber.get(data.recordsNumber.size()-1)).getValue()) : (data.records.get(data.records.size()-data.recordsNumber.get(data.recordsNumber.size()-2)).getValue());
-        int k = 0;
-        Text barText;
+        //int k = 0;
+        //Text barText;
         ArrayList<Bar> barList = new ArrayList<>();
         ArrayList<Rectangle> rect = new ArrayList<Rectangle>();
         ArrayList<Text> barTextList = new ArrayList<Text>();
-        for (int i = 0;(data.type.equals("xml") && i < data.recordsNumber.get(1)) || i < data.recordsNumber.size();i++){
-            if (i != 0){
-                if (data.type.equals("txt")) {
-                    k += data.recordsNumber.get(i-1);
-                }
-                else {
-                    k += data.recordsNumber.get(0);
-                }
-            }
-            for (int j = k; j < k + 10;j++) {
-                Record record = data.records.get(j);
-                Bar bar = new Bar(record.getName(),record.getCategory(),record.getValue(),record.getCountry(),record.getYear(), ((long) (record.getValue() * vBox.getWidth()) / maxValue));
-                barList.add(bar);
-                topTen.add(record);
-                rect.add(bar.bar);
-                barText = new Text(record.getName());
-                barText.setFont(new Font(11));
-                barTextList.add(barText);
-            }
-            for (Record record:topTen) {
-                System.out.println(record.toString());
-            }
-            System.out.println("-----------------------------------------");
-            topTen.clear();
-            barList.clear();
-            if ((data.type.equals("xml") && i != data.recordsNumber.get(1)-1) || data.type.equals("txt") && i != data.recordsNumber.size() -1 ) {
-                rect.clear();
-                barTextList.clear();
-            }
-        }
-        vBox.setSpacing(20);
-        vBox.setStyle("-fx-background-color: #89608E;");
-        vBox.getChildren().addAll(rect);
-        vBoxText.getChildren().addAll(barTextList);
-        vBoxText.setSpacing(30);
-
         ArrayList<Text> categories = new ArrayList<Text>();
         for (int i = 0; i < data.categories.size();i++) {
             categories.add(new Text(data.categories.get(i)));
-            categories.get(i).setFill(Color.rgb(new Random().nextInt(255),new Random().nextInt(255),new Random().nextInt(255)));
+            colorMap.put(data.categories.get(i), colorArray[i]);
+            categories.get(i).setFill(colorMap.get(data.categories.get(i)));
+
+        }
+        Timeline timeline = new Timeline();
+        for (i = 0;(data.type.equals("xml") && i < data.recordsNumber.get(1)) || i <= data.recordsNumber.size();i++){
+
+            vBox.setSpacing(20);
+            vBox.setStyle("-fx-background-color: #89608E;");
+            vBoxText.setSpacing(30);
+
+            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(i * 100), actionEvent -> {
+                if (i >= 0 && i <= data.recordsNumber.size()){
+
+                    if (data.type.equals("txt")) {
+                        k += data.recordsNumber.get(data.recordsNumber.size() - (i));
+                    }
+                    else {
+                        k += data.recordsNumber.get(0);
+                    }
+                }
+                for (int j = k; j < k + 10;j++) {
+                    Record record = data.records.get(j);
+                    Bar bar = new Bar(record.getName(),record.getCategory(),record.getValue(),record.getCountry(),record.getYear(), ((long) (record.getValue() * vBox.getWidth()) / maxValue));
+                    barList.add(bar);
+                    topTen.add(record);
+                    bar.bar.setFill(colorMap.get(bar.getCategory()));
+                    rect.add(bar.bar);
+                    barText = new Text(record.getName());
+                    barText.setFont(new Font(11));
+                    barTextList.add(barText);
+                }
+                for (Record record:topTen) {
+                    System.out.println(record.toString());
+                }
+                System.out.println("-----------------------------------------");
+                vBox.getChildren().addAll(rect);
+                vBoxText.getChildren().addAll(barTextList);
+
+            }));
+
+            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(i * 100 + 100), actionEvent -> {
+
+                    vBox.getChildren().clear();
+                    vBoxText.getChildren().clear();
+
+
+            }));
+
+            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(i * 100 + 100), actionEvent -> {
+
+                    topTen.clear();
+                    barList.clear();
+
+                    rect.clear();
+                    barTextList.clear();
+                    i--;
+            }));
+        }
+        timeline.setCycleCount(1);
+        for (int i = 0; i < 3; i++){
+            timeline.getKeyFrames().remove(timeline.getKeyFrames().size() - 1);
         }
 
+        timeline.play();
+        //timeline.getKeyFrames().clear();
+
+
+        flowPane.getChildren().addAll(categories);
+        flowPane.setHgap(10);
+        flowPane.setVgap(10);
     }
 }
